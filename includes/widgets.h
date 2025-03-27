c030563e8d01e048f27b4ed4c595dcef1aaebfa7
Zea7 <zeaear714@gmail.com>
2025-03-27 20:15:12 +0900

    0327

--- includes/widgets.h
+++ includes/widgets.h
@@ -2,12 +2,15 @@
 #define WIDGETS_H
 
 #define DEFAULT_ROI_COLOR Qt::red
+#define DEFAULT_CHANNEL_SIZE 1024
+#define DEFAULT_CHANNEL_START 0
 
 #define _USE_MATH_DEFINES
 
 #include <vector>
 #include <math.h>
 #include <cmath>
+#include <algorithm>
 
 #include <QWidget>
 
@@ -21,6 +24,10 @@
 #include <QString>
 
 #include <QChart>
+#include <QStackedBarSeries>
+#include <QValueAxis>
+#include <QLogValueAxis>
+#include <QBarSet>
 
 // #include "dialogs.h"
 
@@ -31,7 +38,32 @@ public:
     SpectrumChart();
     ~SpectrumChart();
 
-    
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+
+private:
+    /* 
+
+    */
+    QStackedBarSeries *mainSeries;
+    QValueAxis *axisX;
+    QValueAxis *axisY;
+    QLogValueAxis *logAxisY;
+
+    std::vector<int> maxLevelData;
+
+    int seriesCounter = 0;
+    std::vector<Qt::GlobalColor> chartColorVector = {Qt::blue, Qt::green, Qt::cyan, Qt::yellow, Qt::gray, Qt::black};
+    Qt::GlobalColor roiRegionDefaultColor = DEFAULT_ROI_COLOR;
+    std::vector<std::pair<int, int>> roiRegions;
+
+    int startSample = DEFAULT_CHANNEL_START;
+    int endSample = DEFAULT_CHANNEL_SIZE;
+    int maxMagnitude = 1;
+    int threshold = 0;
+
+    void selectROIRegion(int regionStartPoint, int regionEndPoint);
+    void deselectROIRegion(int regionStartPoint, int regionEndPoint);
 };
 
 class ROITabWidget : public QWidget {
--- sources/UI/spectrumchart_ui.cpp
+++ sources/UI/spectrumchart_ui.cpp
@@ -8,3 +8,39 @@ SpectrumChart::~SpectrumChart() {
 
 }
 
+void SpectrumChart::mousePressEvent(QGraphicsSceneMouseEvent *event){
+
+}
+
+void SpectrumChart::selectROIRegion(int regionStartPoint, int regionEndPoint){
+    if(std::find(roiRegions.begin(), roiRegions.end(), std::make_pair(regionStartPoint, regionEndPoint)) == roiRegions.end())
+        roiRegions.push_back({regionStartPoint, regionEndPoint});
+    else    
+        qDebug() << "ROI Region is already selected!";
+
+    QList<QBarSet *> setList = this->mainSeries->barSets();
+    QList<int> idx;
+
+    for(int i = regionStartPoint; i <= regionEndPoint; i++){
+        idx << i;
+    }
+
+    for(auto *set : setList){
+        set->selectBars(idx);
+    }
+}
+
+void SpectrumChart::deselectROIRegion(int regionStartPoint, int regionEndPoint) {
+    auto iter = std::find(roiRegions.begin(), roiRegions.end(), std::make_pair(regionStartPoint, regionEndPoint));
+
+    if(iter == roiRegions.end())
+        qDebug() << "No ROI Region has same range";
+    else    
+        roiRegions.erase(iter);
+
+    QList<QBarSet *> setList = this->mainSeries->barSets();
+    QList<int> idx;
+
+    // To Be specified
+}
+
