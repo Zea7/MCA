5cc9b64fd6e5b7e52c451c39c59115a814b01d78
Zea7 <zeaear714@gmail.com>
2025-07-28 10:46:31 +0900

    [Design] - #4: Separate String&Style file (07.21)

---
+++ includes/styles.h
@@ -0,0 +1,8 @@
+#ifndef STYLES_H
+#define STYLES_H
+
+#define STYLE_SERIALMANAGER_HINT_TITLE "font-weight: bold"
+#define STYLE_TAB_HINT_TITLE "font-weight: bold"
+#define STYLE_INFOTAB_SERIES_NAME "font-weight: bold"
+
+#endif 
