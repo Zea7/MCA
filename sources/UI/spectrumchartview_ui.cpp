4c8e05a10a2b4c4e012b5ab217ecd1df6b4a1f3f
Zea7 <zeaear714@gmail.com>
2025-07-28 10:48:00 +0900

    [Refactor] - #4: Separate Function file (07.21)

--- sources/UI/spectrumchartview_ui.cpp
+++ sources/UI/spectrumchartview_ui.cpp
@@ -6,23 +6,3 @@ SpectrumChartView::SpectrumChartView(SpectrumChart *chart) : QChartView(chart), 
     this->guideLineItem = chart->scene()->addLine(QLineF(), QPen(Qt::SolidLine));
 }
 
-void SpectrumChartView::mousePressEvent(QMouseEvent *event) {
-    QPointF mousePos = event->pos();
-    QRectF plotArea = chart()->plotArea();
-
-    qreal topY = plotArea.top();     // 가장 위쪽
-    qreal ratio = (mousePos.x() - plotArea.left()) / plotArea.width();  // x는 데이터 기준
-
-    QValueAxis *xAxis = qobject_cast<QValueAxis*>(chart()->axisX());
-    qreal x = xAxis->min() + ratio*(xAxis->max());
-    int index = qRound(x);
-
-    if (index >= 0 && index < qobject_cast<QValueAxis*>(chart()->axisX())->max()){
-        this->guideLineItem->setLine(QLineF(QPointF(mousePos.x(), topY), QPointF(mousePos.x(), plotArea.bottom())));
-        
-        this->mainChart->loggingData(index);
-    }
-    
-
-    QChartView::mousePressEvent(event);
-}
